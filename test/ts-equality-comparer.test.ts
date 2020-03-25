2abba6063a2120d4540adab9de8b0e8638ecc15a
Chris Sperry <christophermartinsperry@gmail.com>
2020-03-25 15:58:51 +0000

    fix(allowDownlevel iteration): update deps and enable downlevelIteration

--- test/ts-equality-comparer.test.ts
+++ test/ts-equality-comparer.test.ts
@@ -1,5 +1,50 @@
+import { deepEqualityComparer } from '../src/ts-equality-comparer'
 describe('Dummy test', () => {
   it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+    const obj1 = [
+      {
+        foo: [1, 2, { bar: { monkey: 4, gorilla: 'ugh' } }]
+      },
+      {
+        woo: ['1,2', { bar: { cat: 4 } }]
+      }
+    ]
+    const obj2 = [
+      {
+        foo: [1, 2, { bar: { monkey: 4, gorilla: 'ugh' } }]
+      },
+      {
+        woo: ['1,2', { bar: { cat: 4 } }]
+      }
+    ]
+    const obj3 = [
+      {
+        foo: [1, 2, { bar: { monkey: 4, lemur: 'ugh' } }]
+      },
+      {
+        woo: ['1,2', { bar: { cat: 4 } }]
+      }
+    ]
+    const obj4 = [
+      {
+        foo: [1, 2, { bar: { gorilla: 'ugh', monkey: 4 } }]
+      },
+      {
+        woo: ['1,2', { bar: { cat: 4 } }]
+      }
+    ]
+    const obj5 = [
+      {
+        foo: [1, 2, { bar: { gorilla: 'ugh', monkey: 4, cat: false } }]
+      },
+      {
+        woo: ['1,2', { bar: { cat: 4 } }]
+      }
+    ]
+
+    expect(deepEqualityComparer.equals(obj1, obj2)).toBeTruthy()
+    expect(deepEqualityComparer.equals(obj1, obj4)).toBeTruthy()
+    expect(deepEqualityComparer.equals(obj1, obj3)).toBeFalsy()
+    expect(deepEqualityComparer.equals(obj4, obj5)).toBeFalsy()
   })
 })
